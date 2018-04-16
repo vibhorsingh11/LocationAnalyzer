30ceaee4f731f69e84907c5d8a2bf3ceaa442e4c
vibhorsingh11 <vibhorsinghcs@gmail.com>
2018-04-17 00:10:32 +0530

    Committing location changes.

--- app/src/main/java/com/phoenix/locationanalyzer/HomeActivity.java
+++ app/src/main/java/com/phoenix/locationanalyzer/HomeActivity.java
@@ -1,9 +1,15 @@
 package com.phoenix.locationanalyzer;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.net.nsd.NsdManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -11,11 +17,24 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
@@ -44,12 +63,16 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.C
             this.googleApiClient.connect();
 
         }
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
                     MY_LOCATION);
+            createLocationRequest();
+
+        }
+        else{
+            createLocationRequest();
         }
 
         checkLocation.setOnClickListener(new View.OnClickListener() {
@@ -64,13 +87,84 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.C
                 }
                 Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
 
-                if(location != null) {
+                if (location != null) {
+                    /*
+                    Place place = PlaceAutocomplete.getPlace(this, data);
+                    LatLng latLng = place.getLatLng();
+                    latitude = (float) latLng.latitude;
+                    longitude = (float) latLng.longitude;*/
+
                     latitude = location.getLatitude();
                     longitude = location.getLongitude();
-                }
 
-                Toast.makeText(HomeActivity.this, String.format("Latitude: %s and Longitude: %s", latitude, longitude),
-                        Toast.LENGTH_LONG).show();
+                    Geocoder geocoder;
+                    List<Address> addresses;
+                    geocoder = new Geocoder(HomeActivity.this, Locale.getDefault());
+
+                    try {
+                        addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+
+                        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                        String city = addresses.get(0).getLocality();
+                        String state = addresses.get(0).getAdminArea();
+                        String country = addresses.get(0).getCountryName();
+                        String postalCode = addresses.get(0).getPostalCode();
+                        String knownName = addresses.get(0).getFeatureName();
+
+                        TextView txview = findViewById(R.id.displayLocation);
+                        txview.setText( address + " " + city + " " +
+                                state + " " + country + " " + postalCode + " " + knownName );
+
+                        Toast.makeText(HomeActivity.this, address + " " + city + " " +
+                                state + " " + country + " " + postalCode + " " + knownName, Toast.LENGTH_LONG).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }                    
+
+                    Toast.makeText(HomeActivity.this, String.format("Latitude: %s and Longitude: %s", latitude, longitude),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+    protected  void createLocationRequest(){
+        final int REQUEST_CHECK_SETTINGS = 199;
+        @SuppressLint("RestrictedApi") LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(HomeActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
             }
         });
     }
